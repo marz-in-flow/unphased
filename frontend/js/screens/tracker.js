be9f0ea7118f72b5df6f67e68561b04c64053051
mnovo30 <70491468+mnovo30@users.noreply.github.com>
2026-05-08 21:09:25 -0400

    Add period log form and logs placeholder

--- frontend/js/screens/tracker.js
+++ frontend/js/screens/tracker.js
@@ -2,9 +2,39 @@ export async function renderTracker(onComplete) {
   const content = document.getElementById("content");
   content.innerHTML = `
     <header>
-      <button id="back-btn">Back</button>
       <h1>Period Tracker</h1>
+      <button type="button" id="back-btn">Back</button>
     </header>
+    
+    <form id="period-log-form">
+      <label for="period-start-date">Add period</label>
+      <input
+          type="date"
+          id="period-start-date"
+          name="periodStartDate"
+          required
+        />
+
+      <label for="notes"> Notes:</label>
+      <textarea 
+        id="notes" 
+        name="notes" 
+        placeholder="Optional" 
+        rows="5" 
+        cols="33"
+      ></textarea>
+      
+      <p id="tracker-error"></p>
+
+      <button type="submit">Save Period</button>
+    </form>
+    
+    <section>
+      <h2>Previous periods</h2>
+      <div id="period-logs-list">
+        <p>Loading period logs...</p>
+      </div>
+    </section>
   `;
 
   document.getElementById("back-btn").addEventListener("click", () => {
